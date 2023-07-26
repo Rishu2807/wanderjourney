72d44dc0973f904e6513a75deaf57c6b74af8ada
Priyanshu0713 <92436602+Priyanshu0713@users.noreply.github.com>
2023-07-26 12:26:42 +0530

    implemented lottie

--- new_project/src/App.js
+++ new_project/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Lottie from 'react-lottie';
+import animationData from './Lottie.json';
 
 function App() {
+  const lottieStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: -1, // Set a negative value to place the Lottie animation behind other elements
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Lottie options={{ animationData }} isClickToPauseDisabled style={lottieStyle} />
+      <div style={{ textAlign: "center" }}>
+        <h1>Hey</h1>
+        <p>This is your content.</p>
+      </div>
+    </>
   );
 }
 
